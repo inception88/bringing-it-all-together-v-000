8464b56e914b38f82f5f147bbc9049452433c294
Benjamin Jones <bljones36@gmail.com>
2018-11-05 00:32:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,4 +54,14 @@ class Dog
     dog = DB[:conn].execute(sql, id)[0]
     self.new(id: dog[0], name: dog[1], breed: dog[2])
   end
+
+  def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?, breed = ?
+    SQL
+
+    dog = DB[:conn].execute(sql, id)[0]
+    self.new(id: dog[0], name: dog[1], breed: dog[2])
+  end
 end
